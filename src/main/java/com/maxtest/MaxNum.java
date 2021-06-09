8bec7f26282b7f4331c9f8d8cfa063385f395d41
tushal kumar <tushalkashyap11@gmail.com>
2021-06-09 09:09:12 -0700

    Refractored: to create generic class in 3 variable

--- src/main/java/com/maxtest/MaxNum.java
+++ src/main/java/com/maxtest/MaxNum.java
@@ -1,14 +1,29 @@
 package com.maxtest;
 
 public class MaxNum {
-    // Method to find max number using compareTo method and Integer object.
-    
-    public  static <T extends Comparable<T>> T testMax(T x, T y, T z){
-        T max = x;
-        if(y.compareTo(max) > 0)
+    public String testMax(String x, String y, String z) {
+        int max = x;
+        if (y.compareTo(max) > 0)
             max = y;
-        if(z.compareTo(max) > 0)
+        if (z.compareTo(max) > 0)
             max = z;
         return max;
     }
+
+    // Method to find max number using compareTo method and Integer object.
+
+    public class MaxNum<T extends Comparable<T>> {
+
+        T x, y, z;
+        T max;
+
+        public MaxNum(T x, T y, T z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+
+    }
 }
+
