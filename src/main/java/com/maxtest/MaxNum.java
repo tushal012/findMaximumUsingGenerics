e892c6e822515e547fa25fec80355e6580c061b7
tushal kumar <tushalkashyap11@gmail.com>
2021-06-09 08:41:24 -0700

    Refactored: code in one generics to find maximum

--- src/main/java/com/maxtest/MaxNum.java
+++ src/main/java/com/maxtest/MaxNum.java
@@ -2,37 +2,13 @@ package com.maxtest;
 
 public class MaxNum {
     // Method to find max number using compareTo method and Integer object.
-    public Integer testMax(Integer x, Integer y, Integer z) {
-        Integer max = x;
-        if (y.compareTo(max) > 0) {
+    
+    public  static <T extends Comparable<T>> T testMax(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0)
             max = y;
-        }
-        if (z.compareTo(max) > 0) {
+        if(z.compareTo(max) > 0)
             max = z;
-        }
-        return max;
-    }
-
-    // Method to find max number using compareTo method and Int object.
-    public Float testMax(Float x, Float y, Float z) {
-        Float max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
-    // Method to find max String
-    public String testMax(String x, String y, String z) {
-        String max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
         return max;
     }
 }
